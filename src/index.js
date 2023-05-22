8b54d4b50333aead505f78f5f1cbea94367b7e37
mjhan <alswn9986@gmail.com>
2023-05-22 14:31:48 +0900

    react basic component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import AppProfile from './AppProfile';
 import reportWebVitals from './reportWebVitals';
+import AppCounter from './AppCounter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <AppProfile /> */}
+    <AppCounter />
   </React.StrictMode>
 );
 
