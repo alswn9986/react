1a86a7a66f6335f2900601a1f153b4fc1f4174c5
mjhan <alswn9986@gmail.com>
2023-05-22 17:58:34 +0900

    counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 
-export default function Counter() {
+export default function Counter({totalCount, onClick}) {
+    // set*() 함수가 호출될 때마다 리액트가 자동으로 Counter() 함수 다시 호출
+    // useState() 훅은 해당 컴포넌트 내에서 다시 호출이 되어도 값을 기억하고 있음
     const [count, setCount] = useState(0);
     return (
         <div className='counter'>
-            <span className='number'>{count}</span>
+            <p className='number'>
+                {count} <span className='total'>/ {totalCount}</span>
+            </p>
             <button
                 className='button'
                 onClick={() => {
-                    setCount(count + 1);
+                    // setCount(count + 1);
+                    setCount((prev) => prev + 1);   // setCount 훅의 마지막 값 prev
+                    onClick();
                 }}
             >Add +</button>
         </div>
