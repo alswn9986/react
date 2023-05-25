e3f92a1ca9d839791dfac193b92f7d41dad1dbd7
mjhan <alswn9986@gmail.com>
2023-05-25 15:36:55 +0900

    로딩, 에러 상태 추가

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,24 +1,33 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Products() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();   // undefined
     const [count, setCount] = useState(0);
     const [products, setProducts] = useState([]);
     const [checked, setChecked] = useState(false);
     const handleChange = () => setChecked((prev) => !prev);
 
     useEffect(() => {
+        setLoading(true);
+        setError(undefined);
         // 상대경로를 쓰면 public에 접근 가능
-        fetch(`data/${checked ? 'sale_' : ''}products.json`)
+        fetch(`data/${checked ? 'sale_' : ''}produdcts.json`)
             .then((res) => res.json())
             .then((data) => {
                 console.log('fetch success');
                 setProducts(data);
-            });
+            })
+            .catch((e) => setError(`에러가 발생했음!!`))
+            .finally(() => setLoading(false));
         // 컴포넌트가 없어질 때 종료 작업이 필요하다면 콜백 함수 작성
         return () => {
             console.log('unmounted');
         }
     }, [checked]);
+
+    if (loading) return <p>...Loading</p>;
+    if (error) return <p>{error}</p>;
 
     return (
         <>
