323286fbc19adbba31940cef40c43186255618d6
mjhan <alswn9986@gmail.com>
2023-06-07 10:23:24 +0900

    add, delete, filter 기능 적용

--- todo/src/TodoApp.jsx
+++ todo/src/TodoApp.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TodoApp.css';
 import { FaSun } from "react-icons/fa";
 import TodoItem from './components/TodoItem';
 
 export default function TodoApp() {
+    const filterList = [{key: 'all', value: 'All'}, {key: 'active', value: 'Active'}, {key: 'completed', value: 'Completed'}];
+    const [activeFilter, setActiveFilter] = useState(filterList.find(x => x.key === 'all'));
+    const handleFilter = (item) => {
+        setActiveFilter(item);
+    };
+
+    const getId = () => new Date().getTime() + Math.random();
+    const [newItem, setNewItem] = useState('');
+    const handleInput = (e) => {
+        setNewItem(e.target.value);
+    };
+
+    const sample = [{
+        id: getId(),
+        status: 'active',
+        content: '강의보기'
+    }, {
+        id: getId(),
+        status: 'completed',
+        content: '카페가기'
+    }];
+    const [list, setList] = useState(sample);
+    const getItemObj = (newItem) => ({
+        id: getId(),
+        status: 'active',
+        content: newItem
+    });
+    const handleAdd = () => {
+        setNewItem('');
+        setList([...list, getItemObj(newItem)]);
+    };
+
+    const handleDelete = (id) => {
+        setList([...list].filter(item => item.id !== id));
+    };
+
     return (
         <div className='container'>
             <div className='header'>
@@ -11,23 +47,26 @@ export default function TodoApp() {
                     <FaSun className='sun' />
                 </div>
                 <div className='tab'>
-                    <a className='filter' href='#'>All</a>
-                    <a className='filter active' href='#'>Active</a>
-                    <a className='filter' href='#'>Completed</a>
+                    {
+                        filterList.map(item => <a key={item.key} className={activeFilter.key === item.key ? 'filter active' : 'filter'} onClick={(e) => {handleFilter(item)}}>{item.value}</a>)
+                    }
                 </div>
             </div>
             <div className='main'>
-                <TodoItem content="산책하기" />
-                <TodoItem content="강의듣기" />
-                <TodoItem content="공부하기" />
+                {
+                    list.map(item => {
+                        if (activeFilter.key === 'all' || item.status === activeFilter.key) {
+                            return (<TodoItem key={item.id} id={item.id} content={item.content} handleDelete={handleDelete} />)
+                        }
+                    })
+                }
             </div>
             <div className='footer'>
                 <div className='addWrap'>
-                    <input placeholder='Add TODO' />
-                    <button>Add</button>
+                    <input placeholder='Add TODO' onChange={handleInput} value={newItem} />
+                    <button onClick={handleAdd}>Add</button>
                 </div>
             </div>
         </div>
     );
 }
-
--- todo/src/components/TodoItem.jsx
+++ todo/src/components/TodoItem.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { FaTrash } from "react-icons/fa";
 
 export default function TodoItem(props) {
+    const handleDelete = () => {
+        props.handleDelete(props.id);
+    }
+
     return (
         <div className='listWrap'>
             <input type='checkbox' />
             <span>{props.content}</span>
-            <FaTrash />
+            <FaTrash onClick={handleDelete} />
         </div>
     );
 }
